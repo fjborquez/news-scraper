017b74ed95952f8c781721a4b19834fb43a3c8c1
Francisco Bórquez <fborquez@outlook.com>
2023-02-12 20:20:05 -0300

    fixes and adds company search

--- news_analyzer_client.py
+++ news_analyzer_client.py
@@ -2,7 +2,7 @@ from analyzer import analyze
 
 
 def search_companies(news):
-    companies = analyze(news['full_article'])
+    companies = analyze(news["full_article"])
 
     if len(companies) > 0:
-        news['companies'] = companies
+        news["companies"] = companies
